3daddb3b01088180ebcece1b94cf649ea76a72c0
Christoph Plattner <Christoph.Plattner@dot.at>
2000-02-02 17:53:05 +0000

    First checkin

---
+++ include/arch/debug_disp.h
@@ -0,0 +1,28 @@
+/*  -*-c-*-
+ * File:		debug_disp.h
+ * Date:		Sun Jan 30 18:41:56 2000
+ * Author:		 (plattner)
+ *  
+ * Abstract:
+ *      Debugging facility over i/o port 
+ * Modifications:
+ * 
+ * $Log: debug_disp.h,v $
+ * Revision 1.1  2000/02/02 17:54:18  plattner
+ * First checkin
+ *
+ *
+ */
+
+#ifndef __debug_disp_h__
+#define __debug_disp_h__
+
+#define DEBUG_DISP_PORT   0x0280
+
+#ifdef DEBUG_DISP
+#define DDISP(v) outb_p(v, DEBUG_DISP_PORT)
+#else
+#define DDISP(v)
+#endif
+
+#endif /* __debug_disp_h__ */
